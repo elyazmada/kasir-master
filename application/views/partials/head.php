eb26738a577f8a7947d3ef35409926fceee55ef8
isinama akun github <isi email akun github>
2021-04-27 17:22:57 +0700

    responsive mobile

--- application/views/partials/head.php
+++ application/views/partials/head.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <!-- Font Awesome -->
 <link rel="stylesheet" href="<?php echo base_url('assets/vendor/adminlte/') ?>plugins/fontawesome-free/css/all.min.css">
 <!-- Theme style -->
